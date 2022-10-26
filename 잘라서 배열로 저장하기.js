8ec5e8f4f0e47f8faed5a3f76864f46bd160fda5
Heesu <heesu0303@naver.com>
2022-10-26 18:10:21 +0900

    Programmers: 10.26

---
+++ 잘라서 배열로 저장하기.js
@@ -0,0 +1,11 @@
+function solution(my_str, n) {
+    // slice(문자열, 배열), splice(배열)
+    var answer = [];
+    for (let i = 0; i < my_str.length; i += n) {
+        answer.push(my_str.slice(i, i + n));
+    }
+
+    return answer;
+}
+
+// my_str.match(new RegExp(`.{1,${n}}`, "g"));
