ea154171e0eaf7008810311d02bc1f99889189f0
Heesu <heesu0303@naver.com>
2022-10-30 23:43:02 +0900

    Programmers: 10.30

---
+++ 옷가게 할인 받기.js
@@ -0,0 +1,7 @@
+function solution(price) {
+    var answer = 0;
+    answer = price >= 500000 ? price * 0.8 : 
+             price >= 300000 ? price * 0.9 :
+             price >= 100000 ? price * 0.95 : price;
+    return ~~answer;
+}
