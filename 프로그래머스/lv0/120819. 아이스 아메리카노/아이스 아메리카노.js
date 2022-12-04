14420455165b3d92f16f159095c47cc72d9bd4f6
heesu <72817156+heesu0303@users.noreply.github.com>
2022-12-04 22:55:38 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -0,0 +1,10 @@
+function solution(money) {
+    var answer = [];
+    
+    let americano = ~~(money / 5500);
+    let changes = money % 5500;
+    
+    answer.push(americano, changes);
+    
+    return answer;
+}
