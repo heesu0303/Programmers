2e2e416745fef58e78421c755c6d3dede0705334
heesu <72817156+heesu0303@users.noreply.github.com>
2022-12-10 02:30:19 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,6 +1,6 @@
 function solution(array) {
     var answer = 0;
     answer = array.sort((a, b) => a - b)
-    let num = answer.length/2-0.5;
+    let num = Math.floor(answer.length/2);
     return answer[num];
 }
